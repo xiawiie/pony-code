85f23d11cf4f674bd1e0783ec8d3e6bf0f687393
xiawiie <106433113+xiawiie@users.noreply.github.com>
2026-08-05 22:02:14 +0800

    Ignore Windows directory attribute-only access

--- pony/security/windows_native.py
+++ pony/security/windows_native.py
@@ -813,11 +813,12 @@ def windows_directory():
 
 def path_is_mutable_by_current_user(path, *, directory, contents=True):
     accesses = (
-        _FILE_WRITE_ATTRIBUTES,
         _DELETE,
         _WRITE_DAC,
         _WRITE_OWNER,
     )
+    if not directory:
+        accesses = (_FILE_WRITE_ATTRIBUTES, *accesses)
     if directory:
         accesses += (_FILE_DELETE_CHILD,)
         if contents:
